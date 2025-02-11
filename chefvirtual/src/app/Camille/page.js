0135d7c5ceee9e81a03e1fc4ccb9c5f5e9ceba1a
julpereira <julia.p@estudante.ifro.edu.br>
2025-02-11 08:47:03 -0400

    commit

--- chefvirtual/src/app/Camille/page.js
+++ chefvirtual/src/app/Camille/page.js
@@ -1,1 +1,10 @@
-a
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Camille() {
+  return (
+    <div>
+      <p>Chefvirtual</p>
+    </div>
+  );
+}
--- chefvirtual/src/app/Guerini/page.js
+++ chefvirtual/src/app/Guerini/page.js
@@ -1,1 +1,10 @@
-a
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Guerini() {
+  return (
+    <div>
+      <p>Chefvirtual</p>
+    </div>
+  );
+}
