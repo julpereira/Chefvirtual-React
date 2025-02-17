5894c2f87887dda04537c8cb1b75506c7673c60e
julpereira <julia.p@estudante.ifro.edu.br>
2025-02-17 15:34:34 -0400

    tela homepage

--- chefvirtual/src/app/julia/homepage/page.js
+++ chefvirtual/src/app/julia/homepage/page.js
@@ -3,25 +3,25 @@ import styles from "@/app/julia/homepage/homepage.module.css";
 
 export default function Homepage() {
   const items = [
-    { name: "Pizza de Pepperoni", author: "Claudia Herz", img: "/img/ppepp.png", link: "/pizza" },
-    { name: "Banoffee", author: "Jordan Elias", img: "/img/banoffe.png", link: "/banoffee" },
-    { name: "Milkshake de café e chocolate", author: "Sofia Ransford", img: "/img/milkshake.png", link: "/milkshake" },
-    { name: "Coxinha", author: "Maria Souza", img: "/img/coxinha.png", link: "/coxinha" },
-    { name: "Torta de Frango", author: "Mayu Kondou", img: "/img/tortafrango.png", link: "/torta-frango" },
-    { name: "Vatapá", author: "Guilherme Barros", img: "/img/vatapa.png", link: "/vatapa" },
-    { name: "Torta de Uva e Maçã", author: "Victoria Steen", img: "/img/tuvamaca.png", link: "/torta-uva" },
-    { name: "Brigadeiro", author: "Carlos Alberto", img: "/img/brigadeiro.png", link: "/brigadeiro" },
-    { name: "Pudim", author: "Vitor Barbosa", img: "/img/pudim.png", link: "/pudim" },
-    { name: "Salpicão", author: "Gabriel Lima", img: "/img/salpicao.png", link: "/salpicao" },
-    { name: "Lasanha", author: "Ana Silva", img: "/img/lasanha.png/", link: "/lasanha" },
-    { name: "Salada Caesar", author: "Marta Rocha", img: "/img/caesarsalad.png", link: "/salada-caesar" },
+    { name: "Pizza de Pepperoni", author: "Claudia Herz", img: "/img/ppepp.png", link: "#" },
+    { name: "Banoffee", author: "Jordan Elias", img: "/img/banoffe.png", link: "#" },
+    { name: "Milkshake de café e chocolate", author: "Sofia Ransford", img: "/img/milkshake.png", link: "#" },
+    { name: "Coxinha", author: "Maria Souza", img: "/img/coxinha.png", link: "#a" },
+    { name: "Torta de Frango", author: "Mayu Kondou", img: "/img/tortafrango.png", link: "#" },
+    { name: "Vatapá", author: "Guilherme Barros", img: "/img/vatapa.png", link: "#" },
+    { name: "Torta de Uva e Maçã", author: "Victoria Steen", img: "/img/tuvamaca.png", link: "#" },
+    { name: "Brigadeiro", author: "Carlos Alberto", img: "/img/brigadeiro.png", link: "#" },
+    { name: "Pudim", author: "Vitor Barbosa", img: "/img/pudim.png", link: "#" },
+    { name: "Salpicão", author: "Gabriel Lima", img: "/img/salpicao.png", link: "#" },
+    { name: "Lasanha", author: "Ana Silva", img: "/img/lasanha.png/", link: "#" },
+    { name: "Salada Caesar", author: "Marta Rocha", img: "/img/caesarsalad.png", link: "#" },
   ];
 
   const categories = [
-    { name: "Doces", img: "/img/doces.png", link: "/doces" },
-    { name: "Salgados", img: "/img/salgados.png", link: "/salgados" },
-    { name: "Comidas Frias", img: "/img/cfrias.png", link: "/comidas-frias" },
-    { name: "Comidas Quentes", img: "/img/cquentes.png", link: "/comidas-quentes" },
+    { name: "Doces", img: "/img/doces.png", link: "#" },
+    { name: "Salgados", img: "/img/salgados.png", link: "#" },
+    { name: "Comidas Frias", img: "/img/cfrias.png", link: "#" },
+    { name: "Comidas Quentes", img: "/img/cquentes.png", link: "#" },
   ];
 
   return (
