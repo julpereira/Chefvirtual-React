a719e66227d5ba773b312eaeba8d46905bcf960b
julpereira <julia.p@estudante.ifro.edu.br>
2025-02-23 15:31:28 -0400

    commit

--- chefvirtual/src/app/julia/perfil/page.js
+++ chefvirtual/src/app/julia/perfil/page.js
@@ -24,7 +24,7 @@ export default function Perfil() {
   };
 
   const confirmDelete = () => {
-    if (password === "1234") {
+    if (password === "") {
       alert("Conta excluída com sucesso!");
       router.push("/julia/homepage");
     } else {
