6d6d5e4f446983e430192d1cfb6acd43d3d31ae8
julpereira <julia.p@estudante.ifro.edu.br>
2025-02-24 14:49:55 -0400

    commit

--- chefvirtual/src/app/Guerini/resulBusca/page.js
+++ chefvirtual/src/app/Guerini/resulBusca/page.js
@@ -1,6 +1,5 @@
-"use client"; // Adiciona para garantir que a página seja renderizada do lado do cliente
-
-import { useState, useEffect, Suspense } from 'react';
+"use client";
+import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 import styles from "./resulBusca.module.css";
 import RecipeCard from "@/components/ReceitaResult";
@@ -16,7 +15,6 @@ const poppinsFont = Poppins({
 export default function ResulBusca() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [query, setQuery] = useState("Nenhuma pesquisa");
-
     const searchParams = useSearchParams();
 
     useEffect(() => {
@@ -30,35 +28,33 @@ export default function ResulBusca() {
     const closeModal = () => setIsModalOpen(false);
 
     return (
-        <Suspense fallback={<div>Carregando...</div>}>
-            <div className={styles.Main}>
-                <div className={styles.headerBusca}>
-                    <h2 className={`${styles.titleResult} ${poppinsFont.className}`}>
-                        20 Resultados para <span className={styles.digitoPesquisa}>{query}</span>
-                    </h2>
+        <div className={styles.Main}>
+            <div className={styles.headerBusca}>
+                <h2 className={`${styles.titleResult} ${poppinsFont.className}`}>
+                    20 Resultados para <span className={styles.digitoPesquisa}>{query}</span>
+                </h2>
 
-                    <button 
-                        onClick={openModal} 
-                        className={styles.filterButton}
-                        aria-label="Filtrar"
-                    >
-                        <SlidersHorizontal size={28} color="#FF914D" />
-                    </button>
-                </div>
-
-                <div className={styles.elementos}>
-                    <RecipeCard />
-                    <RecipeCard />
-                    <RecipeCard />
-                    <RecipeCard />
-                    <RecipeCard />
-                    <RecipeCard />
-                    <RecipeCard />
-                    <RecipeCard />
-                </div>
-
-                <ModalPesquisa isOpen={isModalOpen} onClose={closeModal} />
+                <button 
+                    onClick={openModal} 
+                    className={styles.filterButton}
+                    aria-label="Filtrar"
+                >
+                    <SlidersHorizontal size={28} color="#FF914D" />
+                </button>
             </div>
-        </Suspense>
+
+            <div className={styles.elementos}>
+                <RecipeCard />
+                <RecipeCard />
+                <RecipeCard />
+                <RecipeCard />
+                <RecipeCard />
+                <RecipeCard />
+                <RecipeCard />
+                <RecipeCard />
+            </div>
+
+            <ModalPesquisa isOpen={isModalOpen} onClose={closeModal} />
+        </div>
     );
 }
