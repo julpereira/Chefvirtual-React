53991f78ae279da811157c1272e7182d188097c7
BelchiorIsa <160147840+BelchiorIsa@users.noreply.github.com>
2025-06-13 22:25:14 -0400

    url update

--- chefvirtual/src/app/urls.js
+++ chefvirtual/src/app/urls.js
@@ -1,4 +1,4 @@
-//const valorURL = 'https://chefvirtual.dev.vilhena.ifro.edu.br/api';
-const valorURL = 'http://localhost:9000';
+const valorURL = 'https://chefvirtual.dev.vilhena.ifro.edu.br/api';
+//const valorURL = 'http://localhost:9000';
 
 export default valorURL;
