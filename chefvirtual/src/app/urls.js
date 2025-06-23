8de5a14c666d94860a1c3327854db6ea646bd110
joaobent <joaopedro0744@hotmail.com>
2025-06-22 22:05:03 -0400

    up

--- chefvirtual/src/app/urls.js
+++ chefvirtual/src/app/urls.js
@@ -1,12 +1,3 @@
-let valorURL;
-
-if (typeof window !== 'undefined') {
-  const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-  valorURL = isLocalhost
-    ? 'http://localhost:9000'
-    : 'https://chefvirtual.dev.vilhena.ifro.edu.br/api';
-} else {
-  valorURL = 'https://chefvirtual.dev.vilhena.ifro.edu.br/api'; // fallback no SSR
-}
+const valorURL= 'https://chefvirtual.dev.vilhena.ifro.edu.br/api';
 
 export default valorURL;
