8a43973288d99e0a87447d900665db57a7d48aa6
BelchiorIsa <160147840+BelchiorIsa@users.noreply.github.com>
2025-07-08 17:07:20 -0400

    aaaa

--- chefvirtual/src/app/urls.js
+++ chefvirtual/src/app/urls.js
@@ -1,3 +1,5 @@
-const valorURL= 'https://chefvirtual.dev.vilhena.ifro.edu.br/api';
-
-export default valorURL;
+const valorUrl = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:9000'
+  : 'https://chefvirtual.dev.vilhena.ifro.edu.br/api';
+console.log(valorUrl)
+export default valorUrl;
