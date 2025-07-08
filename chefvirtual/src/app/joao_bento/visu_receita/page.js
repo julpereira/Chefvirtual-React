2678eb6ecb7b30b3eefce5f725abb932250a98f8
BelchiorIsa <160147840+BelchiorIsa@users.noreply.github.com>
2025-07-08 18:55:42 -0400

    g

--- chefvirtual/src/app/joao_bento/visu_receita/page.js
+++ chefvirtual/src/app/joao_bento/visu_receita/page.js
@@ -42,7 +42,7 @@ const App = () => {
 
   useEffect(() => {
     if (!id) return;
-    /*async function getReceitas() {
+    async function getReceitas() {
       try {
         const response = await fetch(valorUrl + '/api/Receitas/GetReceita?idReceita=' + id);
         if (!response.ok) throw new Error('Erro ao buscar receita');
@@ -53,8 +53,10 @@ const App = () => {
       } catch (error) {
         console.error('Erro ao buscar receita:', error);
         alert('Não foi possível carregar a receita.');
+      } finally {
+        setLoading(false);
       }
-    }*/
+    }
 
     async function getComentarios() {
       try {
@@ -64,6 +66,9 @@ const App = () => {
         setComentarios(data);
       } catch (error) {
         console.error('Erro ao buscar comentários:', error);
+      }
+      finally {
+        setLoading(false);
       }
     }
 
@@ -80,7 +85,7 @@ const App = () => {
 
     //getFavoritos();
     getComentarios();
-    //getReceitas();
+    getReceitas();
   }, [id]);
 
   const goBack = () => {
@@ -168,6 +173,10 @@ const App = () => {
     if (!nome) return 'Carregando...';
     const nomeFormatado = nome.charAt(0).toUpperCase() + nome.slice(1).toLowerCase();
     return nomeFormatado;
+  }
+
+  if (loading) {
+    return <div className={styles.container}>Carregando receitas...</div>;
   }
 
   return (
