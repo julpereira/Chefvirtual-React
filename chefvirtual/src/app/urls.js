7eb890321f19032f5af8b33759dedabcde52838c
JoaoGuerini <145067814+JoaoGuerini@users.noreply.github.com>
2025-07-14 03:45:29 -0300

    Local host fudendo

--- chefvirtual/src/app/urls.js
+++ chefvirtual/src/app/urls.js
@@ -1,6 +1,5 @@
-const valorUrl = 
-// process.env.NODE_ENV === 'development'
-  'http://localhost:9000'
-  // : 'https://chefvirtual.dev.vilhena.ifro.edu.br/api';
+const valorUrl = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:9000'
+  : 'https://chefvirtual.dev.vilhena.ifro.edu.br/api';
 console.log(valorUrl)
 export default valorUrl;
